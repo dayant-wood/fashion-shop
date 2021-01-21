5e388b2aba6a0a191fff1d364b5d8d9e165aeead
dayant-wood <dayant.wood@gmail.com>
2021-01-21 18:13:04 +0300

    create: tabs for trending

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
 
+//open mobile-menu  
 var menuButton = $(".menu-button");
 menuButton.on("click", openMenu);
  
   function openMenu () {
     $(".navbar").toggleClass('navbar--visible');
-    $("body").toggleClass("hidden");
-    
-    
-
-    
+    $("body").toggleClass("hidden"); 
+      
   };
  
+//tabs 
+var linkItem = $(".trending__item");
+var contentItem = $(".trending__photos-wrapper"); 
+var colorItem = $(".trending__link");
+  linkItem.on("click", function(event){
+    event.preventDefault();
+    var activeContent= $(this).attr("data-target");
+    contentItem.removeClass('trending__photos-wrapper--active');
+    $(activeContent).toggleClass('trending__photos-wrapper--active');
+    colorItem.removeClass('primary-link');
+    colorItem.toggleClass('primary-link');
+  });
 }); 
