268bb46f9d4add4c0ff0f6a13451aae2fd49b216
dayant-wood <dayant.wood@gmail.com>
2021-01-22 18:08:49 +0300

    create: create section reviews

--- js/main.js
+++ js/main.js
@@ -19,7 +19,41 @@ var colorItem = $(".trending__link");
     var activeContent= $(this).attr("data-target");
     contentItem.removeClass('trending__photos-wrapper--active');
     $(activeContent).toggleClass('trending__photos-wrapper--active');
-    colorItem.removeClass('primary-link');
-    colorItem.toggleClass('primary-link');
+    // colorItem.removeClass('primary-link');
+    // colorItem.toggleClass('primary-link');
+    
   });
+
+
+var mySwiper = new Swiper('.reviews-container', {
+  // Optional parameters
+  
+  loop: true,
+
+  autoplay: {
+    delay: 7000,
+  },
+
+  disableOnInteraction: true,
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+ 
+   keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+});  
+
+$(".reviews-container").hover(
+  function () {
+    this.swiper.autoplay.stop();
+  },
+  function () {
+    this.swiper.autoplay.start();
+  }
+);
 }); 
