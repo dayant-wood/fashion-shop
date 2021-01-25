a2b93ec04f0c6f7eb0130790114d736fcce94211
dayant-wood <dayant.wood@gmail.com>
2021-01-25 11:56:53 +0300

    fixed: fixed everything

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
-//open mobile-menu  
-var menuButton = $(".menu-button");
-menuButton.on("click", openMenu);
+// //open mobile-menu  
+// var menuButton = $(".menu-button");
+// menuButton.on("click", openMenu);
  
-  function openMenu () {
-    $(".navbar").toggleClass('navbar--visible');
-    $("body").toggleClass("hidden"); 
+//   function openMenu () {
+//     $(".navbar").toggleClass('navbar--visible');
+//     $("body").toggleClass("hidden"); 
       
-  };
+//   };
  
 //tabs 
 var linkItem = $(".trending__item");
@@ -24,6 +24,24 @@ var colorItem = $(".trending__link");
     
   });
 
+
+//open mobile-menu  
+var menuButton = $(".menu-button");
+var closeMobileButton = $(".mobile__close");
+menuButton.on("click", openMenu);
+closeMobileButton.on("click", closeMobileMenu);
+ 
+  function openMenu () {
+    $(".mobile").toggleClass('mobile--visible');
+    $("body").toggleClass("hidden"); 
+  }
+  function closeMobileMenu(event) {
+     event.preventDefault();
+    $(".mobile").removeClass('mobile--visible');
+    $("body").toggleClass("hidden"); 
+
+};
+ 
 
 var reviewsSwiper = new Swiper('.reviews-container', {
   // Optional parameters
@@ -57,21 +75,18 @@ $(".reviews-container").hover(
   }
 );
 
-
-var storiesSwiper = new Swiper('.stories-slider', {
+var mySwiper = new Swiper('.stories-swiper', {
   // Optional parameters
-  
-  loop: true,  
+  direction: 'horizontal',
+  loop: false,
 
-  slidesPerView: 2,
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.stories-slider-button--next',
+    prevEl: '.stories-slider-button--prev', 
+    disabledClass: 'stories-slider-button--disabled',
   },
-  
-  
-});
+})
 
 $('#slide').click(storiesSwiper, function(event){
   event.preventDefault();
@@ -83,6 +98,19 @@ $(".form").each(function() {
     $(this).validate({
     errorClass: "invalid",
     messages: {
+    
+    name: {
+      required: "Please, specify your name",
+      minlength: "Name should contain minimum 3 characters",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com",
+    },
+    phone: {
+      required: "Please, specify your phone number",
+      minlength: "Please, enter the full number",
+    },
 
     sub_mail: {
       required: "We need your email address to contact you",
@@ -93,6 +121,36 @@ $(".form").each(function() {
 });
 });
 
+//modal window
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    
+  }
+  
+   function closeModal(event) {
+     event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  
+  $(document).on('keydown', function (d) {
+    if (d.keyCode===27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+  }
+  }); 
 
 
 }); 
